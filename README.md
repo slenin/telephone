5dbe12f0442fb954e29034994b98bf92bc8d7b9d
Lenin Ravindranath Sivalingam <lenin@Lenins-MacBook-Pro.local>
2025-03-16 00:55:52 -0700

    first version of telephone

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# telephone
-Game of telephone with AI
+
+## Create new python environment
+python3 -m venv env
+
+## Activate the environment
+source env/bin/activate
+
+## Select python interpreter in VS Code
+Ctrl + Shift + P and select interpreter to be env/bin/python
+
+## Install depedencies
+pip install -r requirement.txt
+
+## Create a .env file in the folder with the following content
+OPENAI_API_KEY=<key here>
+HYPEBOLIC_API_KEY=<key here>
+
+## Run the code
+Run from VS Code
---
+++ main.py
@@ -0,0 +1,78 @@
+import requests
+from PIL import Image
+import base64
+from io import BytesIO
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+hyperbolic_key = os.getenv("HYPEBOLIC_API_KEY")
+openai_key = os.getenv("OPENAI_API_KEY")
+
+
+def generate_image(prompt):
+    url = "https://api.hyperbolic.xyz/v1/image/generation"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {hyperbolic_key}"
+    }
+    data = {
+        "model_name": "FLUX.1-dev",
+        "prompt": prompt,
+        "steps": 30,
+        "cfg_scale": 5,
+        "enable_refiner": False,
+        "height": 1024,
+        "width": 1024,
+        "backend": "auto"
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    data = response.json()
+    return data["images"][0]["image"]
+
+
+def show_image(image_base64):
+    image_data = base64.b64decode(image_base64)
+    image = Image.open(BytesIO(image_data))
+    image.show()
+
+
+def caption_image(image_base64):
+    client = OpenAI(api_key=openai_key)
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Generate a descriptive caption for the attached image.",
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": f"data:image/jpeg;base64,{image_base64}"},
+                        },
+                    ],
+                }
+            ]
+        )
+        caption = response.choices[0].message.content
+        return caption
+    except Exception as e:
+        print("Error during captioning:", e)
+        return None
+
+
+caption = "Two cats, a dog, and a monkey sitting around a dining table with a beautiful sunset in the background."
+for i in range(5):
+    image_base64 = generate_image(caption)
+    show_image(image_base64)
+    caption = caption_image(image_base64)
+    print(caption)
+    caption = caption + " Produce an image that is exactly opposite of this description."
+    print("-----")
