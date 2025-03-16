c89c5d1015b8328c8561e3de6227d5b6f17230ae
Lenin Ravindranath Sivalingam <lenin@Lenins-MacBook-Pro.local>
2025-03-16 01:31:28 -0700

    image to image

--- main.py
+++ main.py
@@ -12,6 +12,30 @@ hyperbolic_key = os.getenv("HYPEBOLIC_API_KEY")
 openai_key = os.getenv("OPENAI_API_KEY")
 
 
+def generate_image_with_image(prompt, image_base64):
+    url = "https://api.hyperbolic.xyz/v1/image/generation"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {hyperbolic_key}"
+    }
+    data = {
+        "model_name": "SDXL1.0-base",
+        "prompt": prompt,
+        "steps": 30,
+        "cfg_scale": 5,
+        "enable_refiner": False,
+        "height": 1024,
+        "width": 1024,
+        "enable_reference": True,
+        "image": image_base64,
+        "strength": 0.8,
+        "backend": "auto"
+    }
+    response = requests.post(url, headers=headers, json=data)
+    data = response.json()
+    return data["images"][0]["image"]
+
+
 def generate_image(prompt):
     url = "https://api.hyperbolic.xyz/v1/image/generation"
     headers = {
@@ -28,7 +52,6 @@ def generate_image(prompt):
         "width": 1024,
         "backend": "auto"
     }
-    
     response = requests.post(url, headers=headers, json=data)
     data = response.json()
     return data["images"][0]["image"]
@@ -68,11 +91,24 @@ def caption_image(image_base64):
         return None
 
 
-caption = "Two cats, a dog, and a monkey sitting around a dining table with a beautiful sunset in the background."
-for i in range(5):
-    image_base64 = generate_image(caption)
-    show_image(image_base64)
-    caption = caption_image(image_base64)
-    print(caption)
-    caption = caption + " Produce an image that is exactly opposite of this description."
-    print("-----")
+def func1():
+    prompt1 = "A beautiful sunrise."
+    reference_image = generate_image(prompt1)
+    show_image(reference_image)
+    prompt2 = "Put two cats in the image"
+    final_image = generate_image_with_image(prompt2, reference_image)
+    show_image(final_image)
+
+
+def func2():
+    caption = "Two cats riding a motorbike."
+    for i in range(5):
+        image_base64 = generate_image(caption)
+        show_image(image_base64)
+        caption = caption_image(image_base64)
+        print(caption)
+        caption = caption + " Produce an image that is exactly opposite of this description."
+        print("-----")
+
+
+func1()
